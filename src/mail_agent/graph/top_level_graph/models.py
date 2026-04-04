b0ad4d1b0a2e1f760745da29fb9fb2dadafe6bbb
Desantnikov <desiatnikovwork@gmail.com>
2026-04-04 14:27:21 +0200

    feat: postgres checkpointer; human in the loop works

--- src/mail_agent/graph/top_level_graph/models.py
+++ src/mail_agent/graph/top_level_graph/models.py
@@ -7,4 +7,7 @@ class NodesEnum(str, Enum):
     BUILD_EXECUTION_PLAN = "build_execution_plan"
     DISPATCH_EXECUTION_PLAN_STEP = "dispatch_execution_plan_step"
 
+    LOCAL_ACTION_SUBGRAPH = "local_action_subgraph"
+    REPLY_SUBGRAPH = "reply_subgraph"
+
     SHUTDOWN = "shutdown"
--- src/mail_agent/graph/top_level_graph/nodes/build_execution_plan.py
+++ src/mail_agent/graph/top_level_graph/nodes/build_execution_plan.py
@@ -3,9 +3,9 @@ from src.mail_agent.services.contracts import ExecutionPlanBuilderInput
 from src.mail_agent.states.top_level_state import TopLevelState
 
 
-def build_execution_plan(state: TopLevelState):
+async def build_execution_plan(state: TopLevelState):
     execution_plan_builder_input = ExecutionPlanBuilderInput(email_data=state.email_data)
     execution_plan_builder = ExecutionPlanBuilder(execution_plan_builder_input)
-    execution_plan = execution_plan_builder.build_execution_plan()
+    execution_plan = await execution_plan_builder.build_execution_plan()
 
     return {'execution_plan': execution_plan}
